c1acf01f179c849225c34f0835ec271d0df0f9d6
Mauro Donnantuoni Moratto <maurodonnamora@gmail.com>
2024-09-13 22:18:35 -0300

    Termina formulario de info de perfil

--- src/app/main-frame/profile/info/info.component.ts
+++ src/app/main-frame/profile/info/info.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core'
 import { FormsModule } from '@angular/forms'
-import { StubLoginService, User } from '../../../../model/User'
+import { Calculator, Cautious, Claimant, Experiencied, GreatReader, Inconstant, Nativist, Polyglot, readerModes, SearchCriteria, StubLoginService, User } from '../../../../model/User'
 import { CommonModule } from '@angular/common'
 
 
@@ -13,15 +13,97 @@ import { CommonModule } from '@angular/common'
 })
 export class InfoComponent {
   user: User = new User( '', '', '', new Date(), '', 0 )
+  readModes = readerModes
+  searchCriteria: Record<
+    string,{ 
+      checked: boolean, 
+      criteria: SearchCriteria, 
+      requireNumberParam: boolean,
+      displayName: string 
+    }
+  > = {}
+  
 
   constructor( public loginService: StubLoginService ) {}
 
   ngOnInit() {
     this.user = this.loginService.getSignedUser()!
+    this.resetSearchCriteria()
+  }
+  
+
+  resetSearchCriteria() {
+    this.searchCriteria = {
+      cautious: {
+        checked: Boolean(this.user.searchCriteria.find( cri => cri instanceof Cautious )),
+        criteria: new Cautious(),
+        requireNumberParam: false,
+        displayName: "Precavido"
+      },
+      claiment: {
+        checked: Boolean(this.user.searchCriteria.find( cri => cri instanceof Claimant )),
+        criteria: new Claimant(),
+        requireNumberParam: false,
+        displayName: "Demandante"
+      },
+      inconstant: {
+        checked: Boolean(this.user.searchCriteria.find( cri => cri instanceof Inconstant )),
+        criteria: new Inconstant(),
+        requireNumberParam: true,
+        displayName: "Cambiante"
+      },
+      greatReader: {
+        checked: Boolean(this.user.searchCriteria.find( cri => cri instanceof GreatReader )),
+        criteria: new GreatReader(),
+        requireNumberParam: false,
+        displayName: "Leedor"
+      },
+      nativist: {
+        checked: Boolean(this.user.searchCriteria.find( cri => cri instanceof Nativist )),
+        criteria: new Nativist(),
+        requireNumberParam: false,
+        displayName: "Nativista"
+      },
+      polyglot: {
+        checked: Boolean(this.user.searchCriteria.find( cri => cri instanceof Polyglot )),
+        criteria: new Polyglot(),
+        requireNumberParam: false,
+        displayName: "Políglota"
+      },
+      experiencied: {
+        checked: Boolean(this.user.searchCriteria.find( cri => cri instanceof Experiencied )),
+        criteria: new Experiencied(),
+        requireNumberParam: false,
+        displayName: "Experimentado"
+      },
+      calculator: {
+        checked: Boolean(this.user.searchCriteria.find( cri => cri instanceof Calculator )),
+        criteria: new Calculator(),
+        requireNumberParam: true,
+        displayName: "Calculador"
+      }
+    }
+  }
+
+  setBirthday = ( e: Event ) => { 
+    this.user.birthday = new Date( (e.target as HTMLInputElement).value )
+  }
+
+  updateSearchCriteria( criteria: string) {
+    this.searchCriteria[ criteria ].checked = !this.searchCriteria[ criteria ].checked
+    this.user.searchCriteria = Object.values( this.searchCriteria ).filter(
+      cri => cri.checked
+    ).map( cri => cri.criteria )
+  }
+
+  cancelUserEdit() {
+    this.user = this.loginService.getSignedUser()!
+    this.resetSearchCriteria()
   }
 
   saveUserInfo() {
     this.loginService.updateSignedUserData( this.user )
+    alert( JSON.stringify( this.user ) )
   }
 }
 
--- src/model/User.ts
+++ src/model/User.ts
@@ -1,4 +1,6 @@
 import { Injectable } from "@angular/core"
+import { Book } from "./Book"
+import { Recommendation } from "./Recommendation"
 
 
 export class User {
@@ -7,7 +9,7 @@ export class User {
     username: string
     birthday: Date
     email: string
-    readTimeAvg: number
+    readTimeMinAvg: number
     readMode: ReadMode
     searchCriteria: SearchCriteria[]
 
@@ -17,18 +19,26 @@ export class User {
         username: string, 
         birthday: Date, 
         email: string, 
-        readTimeAvg: number,
-        readMode = new ReaderAvg(), 
-        searchCriteria = []
+        readTimeMinAvg: number,
+        readMode = readerModes.avgReader, 
+        searchCriteria = [],
     ) {
         this.lastName = lastName
         this.firstName = firstName
         this.username = username
         this.birthday = birthday
         this.email = email
-        this.readTimeAvg = readTimeAvg
+        this.readTimeMinAvg = readTimeMinAvg
         this.readMode = readMode
         this.searchCriteria = searchCriteria
+    }
+
+    baseReadTime( book: Book ) {
+        return book.pages / this.readTimeMinAvg
+    }
+
+    readTime( book: Book ) {
+        return this.readMode.readTime( book, this )
     }
   }
 
@@ -113,23 +123,122 @@ export class StubLoginService {
             this.signedUser?.username || '',
             this.signedUser?.birthday || new Date(),
             this.signedUser?.email || '',
-            this.signedUser?.readTimeAvg || 0
+            this.signedUser?.readTimeMinAvg || 0
         )
     }
 
     updateSignedUserData( newUserData: User ) {
-        this.signedUser = newUserData
+        this.signedUser = new User(
+            newUserData?.lastName || '',
+            newUserData?.firstName || '',
+            newUserData?.username || '',
+            newUserData?.birthday || new Date(),
+            newUserData?.email || '',
+            newUserData?.readTimeMinAvg || 0
+        )
     }
 }
 
-export interface ReadMode {
-    algoParaQueCompile: string
+interface ReadMode {
+    readTime( libro: Book, usuario: User ): number
+}
+
+
+class AvgReader implements ReadMode {
+    // Para debug... ELIMINAR
+    readMode = "AvgReader"
+
+    readTime( book: Book, user: User ) { return user.baseReadTime( book ) }
+}
+
+
+// TODO: Implementar los método correctos
+class AnxiousReader implements ReadMode {
+    // Para debug... ELIMINAR
+    readMode = "AnxiousReader"
+
+    readTime( book: Book, user: User ) { return user.baseReadTime( book ) }
+}
+
+
+class FanaticReader implements ReadMode {
+    // Para debug... ELIMINAR
+    readMode = "FanaticReader"
+
+    readTime( book: Book, user: User ) { return user.baseReadTime( book ) }
+}
+
+
+class RecurrentReader implements ReadMode {
+    // Para debug... ELIMINAR
+    readMode = "RecurrentReader"
+
+    readTime( book: Book, user: User ) { return user.baseReadTime( book ) }
+}
+
+export const readerModes = {
+    avgReader: new AvgReader(),
+    anxiousReader: new AnxiousReader(),
+    fanaticReader: new FanaticReader(),
+    recurrentReader: new RecurrentReader()
 }
 
 export interface SearchCriteria {
-    algoParaQueCompile: string
+    isAdvisable( recom: Recommendation ): boolean
 }
 
-export class ReaderAvg implements ReadMode {
-    algoParaQueCompile = "Compilá!!"
+export class Cautious implements SearchCriteria {
+    // Para debug... ELIMINAR
+    criteria = "cautious"
+    // TODO: Implementar algún día
+    isAdvisable( recom: Recommendation ) { return recom && true }
+}
+
+export class Claimant implements SearchCriteria {
+    // Para debug... ELIMINAR
+    criteria = "claimant"
+    // TODO: Implementar algún día
+    isAdvisable( recom: Recommendation ) { return recom && true }
+}
+
+export class GreatReader implements SearchCriteria {
+    // Para debug... ELIMINAR
+    criteria = "greatReader"
+    // TODO: Implementar algún día
+    isAdvisable( recom: Recommendation ) { return recom && true }
+}
+
+export class Nativist implements SearchCriteria {
+    // Para debug... ELIMINAR
+    criteria = "nativist"
+    // TODO: Implementar algún día
+    isAdvisable( recom: Recommendation ) { return recom && true }
+}
+
+export class Polyglot implements SearchCriteria {
+    // Para debug... ELIMINAR
+    criteria = "polyglot"
+    // TODO: Implementar algún día
+    isAdvisable( recom: Recommendation ) { return recom && true }
+}
+
+export class Inconstant implements SearchCriteria {
+    // Para debug... ELIMINAR
+    criteria = "inconstant"
+    // TODO: Implementar algún día
+    isAdvisable( recom: Recommendation ) { return recom && true }
+}
+
+export class Experiencied implements SearchCriteria {
+    // Para debug... ELIMINAR
+    criteria = "experiencied"
+    // TODO: Implementar algún día
+    isAdvisable( recom: Recommendation ) { return recom && true }
+}
+
+export class Calculator implements SearchCriteria {
+    // Para debug... ELIMINAR
+    criteria = "calculator"
+    // TODO: Implementar algún día
+    isAdvisable( recom: Recommendation ) { return recom && true }
 }
