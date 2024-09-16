8a549e9f8c8cec8b14f9c709fbcc6efdc2915917
Diaz Matias Hernan <diaz.matiash@gmail.com>
2024-09-16 19:27:28 -0300

    por mergear develop

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component , Input } from '@angular/core'
+import { Component  } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { FormsModule } from '@angular/forms'
 
@@ -11,6 +11,6 @@ import { FormsModule } from '@angular/forms'
 })
 export class SearchBarComponent {
 
-  @Input() showCheck: boolean = false
+  myRecomsFlag: boolean = location.pathname.includes('myrecoms')
 
 }
--- src/app/main-frame/recommendations/recommendations.component.ts
+++ src/app/main-frame/recommendations/recommendations.component.ts
@@ -13,6 +13,6 @@ import {  RouterOutlet } from '@angular/router'
 })
 export class RecommendationsScreen {
 
-  myRecomsFlag: boolean = location.pathname.includes('myrecoms')
+  
   
 }
