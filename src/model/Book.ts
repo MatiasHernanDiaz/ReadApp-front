76a241d4bce9a0a119f62190ba9cd6bba5cde757
Theo <narmontastheo@gmail.com>
2024-09-16 21:55:36 -0300

    book completo

--- src/model/Book.ts
+++ src/model/Book.ts
@@ -1,6 +1,14 @@
 
 export class Book {
-    pages: number
+
+    pages: number = 0
+    title: string = ""
+    imageURL: string = ""
+    autor: string = ""
+    words: number = 0
+    date: Date = new Date(0,0,0)
+    lenguages: string = ""
+    sales: number = 0
 
     constructor( pages: number ) {
         this.pages = pages
