9917b689ed643472a07b8c5fd893542469cbb3e1
lautaCuellar <lautilh69@gmail.com>
2024-09-19 16:50:50 -0300

    amigos son los amigos
    agregue las imagenes de todos los personajes, tambien agregue personajes para poder intercambiar amigos segun el usuario logeado, implemente la logica para que me devuelva los usuarios que son amigos, cada amigo es tambien un usuario, hice en el archivo users (model) un constructor para crear los usuarios, luego un mapa para acceder a los usuarios segun su username, ya que esto es lo que despues le voy a pasar en friend a ca usuario( array de otros usuarios "amigos") y despues le asigno los amigos , esto es en el getUser, este metodo dsp lo uso en el ts para ver cuales son los amigos de cada uno

--- src/app/main-frame/profile/friends/friends.component.ts
+++ src/app/main-frame/profile/friends/friends.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { StubLoginService } from '@src/model/User'
+import { User } from '@src/model/User'
 
 @Component({
   selector: 'app-friends',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './friends.component.html',
-  styleUrl: './friends.component.css'
+  styleUrls: ['./friends.component.css']
 })
-export class FriendsComponent {
+export class FriendsComponent implements OnInit {
+  users: User[] = []
+  friends: User[] = []
 
+  constructor(private userService: StubLoginService) {}
+
+  ngOnInit(): void {
+    const currentUser = this.userService.getSignedUser()
+    console.log("Current User:", currentUser)     
+      if (currentUser.friends) {
+        // Crear un conjunto de usernames de amigos para búsqueda rápida
+        const friendUsernames = new Set(currentUser.friends.map(friend => friend.username))
+        
+        // Filtrar usuarios para encontrar amigos
+        this.friends = this.userService.getUsers().filter(user => {
+          const isFriend = friendUsernames.has(user.username)
+          return isFriend 
+        })
+      }
+  }
 }
