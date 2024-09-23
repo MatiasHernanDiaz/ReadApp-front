1c1e68094dbdac77003b91832596454c7243d9c8
gasabe <gabelardo@estudiantes.unsam.edu.ar>
2024-09-22 21:40:00 -0300

    correcion de diseño de recomendaciones

--- src/app/components/recom/recom.component.ts
+++ src/app/components/recom/recom.component.ts
@@ -1,49 +1,31 @@
-import { Component } from '@angular/core'
-import { bootstrapBookmark, bootstrapStar, bootstrapBook, bootstrapClock,
-   bootstrapHeart,bootstrapArrowRight,bootstrapTrash } from '@ng-icons/bootstrap-icons'
+import { Component, Input, Output, EventEmitter } from '@angular/core'
+import { bootstrapBookmark, bootstrapStar, bootstrapBook, 
+  bootstrapClock, bootstrapHeart, bootstrapArrowRight, bootstrapTrash } from '@ng-icons/bootstrap-icons'
 import { NgIconComponent, provideIcons } from '@ng-icons/core'
 import { heroUsers } from '@ng-icons/heroicons/outline'
 import { CommonModule } from '@angular/common'
 import { Router } from '@angular/router'
-import { RemoveRecomComponent } from '../remove-recom/remove-recom.component'
-import { Output, EventEmitter,Input } from '@angular/core'
-import { RecommendationService } from '@src/app/services/recom.service'
 import { Recommendation } from '@src/model/Recommendation'
 
 @Component({
   selector: 'app-recom',
   standalone: true,
-  imports: [RemoveRecomComponent,NgIconComponent,CommonModule],
-  viewProviders: [provideIcons({  heroUsers,bootstrapBookmark, bootstrapStar,
-     bootstrapBook, bootstrapClock, bootstrapHeart,bootstrapArrowRight,bootstrapTrash })],
+  imports: [NgIconComponent, CommonModule],
+  viewProviders: [provideIcons({ heroUsers, bootstrapBookmark, bootstrapStar, bootstrapBook, bootstrapClock, bootstrapHeart, bootstrapArrowRight, bootstrapTrash })],
   templateUrl: './recom.component.html',
-  styleUrl: './recom.component.css'
+  styleUrls: ['./recom.component.css']
 })
 export class RecomComponent {
-  @Input() recommendations: Recommendation[] = []
+  @Input() recommendation!: Recommendation //  solo una recomendación
   @Output() onDeleteRecom = new EventEmitter<number>()
 
-  constructor(private router: Router,private recommendationService: RecommendationService) {}
-  
-  async ngOnInit() {
-    this.recommendations = await this.recommendationService.getRecommendations()
-  }
+  constructor(private router: Router) {}
 
   goToDetail(id: number) {
-    console.log('id',id)
-    console.log(this.router)
-    this.router.navigate(['app/recoms/recom-details/',id])
+    this.router.navigate(['app/recoms/recom-details/', id])
   }
 
- 
-
-removeRecom(id: number): void {
-console.log(id)
-this.onDeleteRecom.emit(id) // consolea la recomendación
-
-
-
-}
-
-
+  removeRecom(id: number): void {
+    this.onDeleteRecom.emit(id)
+  }
 }
