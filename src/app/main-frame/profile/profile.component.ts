c8e42aa41d7cd2d23e09438f06a440a10a75d4a2
Mauro Donnantuoni Moratto <maurodonnamora@gmail.com>
2024-09-28 21:46:09 -0300

    Toma datos del usuario en el aside del profile

--- src/app/main-frame/profile/profile.component.ts
+++ src/app/main-frame/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router'
+import { StubLoginService, User } from '@src/model/User'
 
 @Component({
   selector: 'profile-screen',
@@ -10,5 +11,13 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router'
   styleUrl: './profile.component.css'
 })
 export class ProfileScreen {
+  user: User = new User( '', '', '', new Date(), '',[],[],[], 0 , )
+  click: boolean = false
+  dropdown: string = "hide"
   
+  constructor( public loginService: StubLoginService ) {}
+
+  ngOnInit() {
+    this.user = this.loginService.getSignedUser()!
+  }
 }
