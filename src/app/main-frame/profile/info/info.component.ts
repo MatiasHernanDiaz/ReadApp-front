a439aa125ad14ecb23019527c6e360776e2bcc63
Mauro Donnantuoni Moratto <maurodonnamora@gmail.com>
2024-10-14 00:48:24 -0300

    Parseo de ReadMode y SearchCriteria

--- src/app/main-frame/profile/info/info.component.ts
+++ src/app/main-frame/profile/info/info.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { 
-  Calculator, Cautious, Claimant, Experiencied, GreatReader, Inconstant, Language, Nativist, Polyglot, 
+  Calculator, Cautious, Claimant, Combined, Experiencied, GreatReader, Inconstant, Language, Nativist, Polyglot, 
   readerModes, SearchCriteria, User 
 } from '@src/app/model/User'
 import { CommonModule } from '@angular/common'
@@ -18,7 +18,7 @@ import { LoginService } from '@src/app/services/Login/login.service'
   styleUrl: './info.component.css'
 })
 export class InfoComponent {
-  user: User = new User(0, '', '', '', new Date(),'', Language.SPANISH,[],[],[], 0 )
+  user: User = new User(-1, '', '', '', new Date(),'', Language.SPANISH,[],[],[], 0 )
   readModes = readerModes
   readModeChecked = this.user.readMode.toCustomString()
   searchCriteria: Record<
@@ -44,42 +44,42 @@ export class InfoComponent {
   resetSearchCriteria() {
     this.searchCriteria = {
       cautious: {
-        checked: Boolean(this.user.searchCriteria.find( cri => cri.toCustomString() === 'Precavido' )),
+        checked: Boolean(this.user.searchCriteria.toCustomString() === 'Precavido' ),
         criteria: new Cautious(),
         requireNumberParam: false
       },
       claiment: {
-        checked: Boolean(this.user.searchCriteria.find( cri => cri.toCustomString() === 'Demandante' )),
+        checked: Boolean(this.user.searchCriteria.toCustomString() === 'Demandante' ),
         criteria: new Claimant(),
         requireNumberParam: false
       },
       inconstant: {
-        checked: Boolean(this.user.searchCriteria.find( cri => cri.toCustomString() === 'Cambiante' )),
+        checked: Boolean(this.user.searchCriteria.toCustomString() === 'Cambiante' ),
         criteria: new Inconstant(),
         requireNumberParam: true
       },
       greatReader: {
-        checked: Boolean(this.user.searchCriteria.find( cri => cri.toCustomString() === 'Leedor' )),
+        checked: Boolean(this.user.searchCriteria.toCustomString() === 'Leedor' ),
         criteria: new GreatReader(),
         requireNumberParam: false
       },
       nativist: {
-        checked: Boolean(this.user.searchCriteria.find( cri => cri.toCustomString() === 'Nativista' )),
+        checked: Boolean(this.user.searchCriteria.toCustomString() === 'Nativista' ),
         criteria: new Nativist(),
         requireNumberParam: false
       },
       polyglot: {
-        checked: Boolean(this.user.searchCriteria.find( cri => cri.toCustomString() === 'Políglota' )),
+        checked: Boolean(this.user.searchCriteria.toCustomString() === 'Políglota' ),
         criteria: new Polyglot(),
         requireNumberParam: false
       },
       experiencied: {
-        checked: Boolean(this.user.searchCriteria.find( cri => cri.toCustomString() === 'Experimentado' )),
+        checked: Boolean(this.user.searchCriteria.toCustomString() === 'Experimentado' ),
         criteria: new Experiencied(),
         requireNumberParam: false
       },
       calculator: {
-        checked: Boolean(this.user.searchCriteria.find( cri => cri.toCustomString() === 'Calculador' )),
+        checked: Boolean(this.user.searchCriteria.toCustomString() === 'Calculador' ),
         criteria: new Calculator(),
         requireNumberParam: true
       }
@@ -92,9 +92,11 @@ export class InfoComponent {
 
   updateSearchCriteria( criteria: string) {
     this.searchCriteria[ criteria ].checked = !this.searchCriteria[ criteria ].checked
-    this.user.searchCriteria = Object.values( this.searchCriteria ).filter(
+
+    const criterias = Object.values( this.searchCriteria ).filter(
       cri => cri.checked
     ).map( cri => cri.criteria )
+    this.user.searchCriteria = criterias.length > 1 ? new Combined( criterias ) : criterias[0]
   }
 
   cancelUserEdit() {
