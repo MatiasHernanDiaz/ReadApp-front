db5221262dcd16972824e059acb7b60f288780f8
Diaz Matias Hernan <diaz.matiash@gmail.com>
2024-10-14 17:34:41 -0300

    se agrega creacion de valoraciones y un par de botones, se necesita mucho para terminar esto!!

--- src/app/components/add-rating/add-rating.component.spec.ts
+++ src/app/components/add-rating/add-rating.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { HttpClient } from '@angular/common/http'
 import { AddRatingComponent } from './add-rating.component'
 import { provideRouter } from '@angular/router'
-import { LoginService } from '@src/app/login/login.component'
+import { LoginService } from '@src/app/services/Login/login.service'
 import { routes } from '@src/app/app.routes'
 import { httpClientSpy, loginServiceStub } from '@src/app/services/serviceStubs'
 
